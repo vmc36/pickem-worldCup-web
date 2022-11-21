10e5a89b0e95ef7af7a6e6b6216783dca10941c3
Vinicius <vmcmodesto@gmail.com>
2022-11-21 20:02:10 -0300

    fix: leaderboard ranking and user icon

--- src/screens/Leaderboard/components/LeaderboardRow.jsx
+++ src/screens/Leaderboard/components/LeaderboardRow.jsx
@@ -1,74 +1,70 @@
-import { tail, tailed } from 'tailed-js'
-import styledBy from 'styled-by'
-import { Link } from 'react-router-dom'
+import { tail, tailed } from "tailed-js";
+import styledBy from "styled-by";
+import { Link } from "react-router-dom";
 
-const LeaderboardRowWrapper = tailed('tr')`
+const LeaderboardRowWrapper = tailed("tr")`
     border-l-8
     p-4
 
-    ${styledBy('position', {
-        1: 'border-amber-400',
-        2: 'border-zinc-400',
-        3: 'border-yellow-700'
+    ${styledBy("position", {
+      1: "border-amber-400",
+      2: "border-zinc-400",
+      3: "border-yellow-700",
     })}
 
-    ${styledBy('type', {
-        'default': 'even:bg-slate-200',
-        'user': 'border-primary bg-primary text-on-primary'
+    ${styledBy("type", {
+      default: "even:bg-slate-200",
+      user: "border-primary bg-primary text-on-primary",
     })}
-`
+`;
 
 export function LeaderboardRowDivider() {
-    return (
-        <LeaderboardRowWrapper>
-            <td className="text-2xl" colSpan={5}>
-                ...
-            </td>
-        </LeaderboardRowWrapper>
-    )
+  return (
+    <LeaderboardRowWrapper>
+      <td className="text-2xl" colSpan={5}>
+        ...
+      </td>
+    </LeaderboardRowWrapper>
+  );
 }
 
-export function LeaderboardRow({score, isCurrentUser}) {
-    const type = isCurrentUser ? 'user' : 'default'
-    const performancePercentual = `${(score.performance * 100).toFixed(1)}%`
+export function LeaderboardRow({ score, isCurrentUser }) {
+  const type = isCurrentUser ? "user" : "default";
+  const performancePercentual = `${(score.performance * 100).toFixed(1)}%`;
 
-    const linkClassNames = tail`
+  const linkClassNames = tail`
         font-bold
 
-        ${(props) => props.type === 'user' ? 'text-on-primary' : 'text-primary'}
-    `
+        ${(props) =>
+          props.type === "user" ? "text-on-primary" : "text-primary"}
+    `;
 
-    return (
-        <LeaderboardRowWrapper
-            position={score.position}
-            type={type}
-            >
-            <td className="text-sm font-bold">{score.position}</td>
-            <td>{score.user.username}</td>
-            <td>{score.points}</td>
-            <td className="flex flex-col items-center justify-center">
-                <span className="inline text-sm">
-                    {performancePercentual}
-                </span>
-                <meter
-                    low={0.35}
-                    high={0.7}
-                    optimum={1}
-                    min={0}
-                    max={1}
-                    value={score.performance}
-                    >
-                        {performancePercentual}
-                </meter>
-            </td>
-            <td>
-                <Link
-                    to={`/u/${score.user.username}`}
-                    className={linkClassNames({type})}
-                    >
-                    Ver Perfil
-                </Link>
-            </td>
-        </LeaderboardRowWrapper>
-    )
+  return (
+    <LeaderboardRowWrapper position={score.position} type={type}>
+      <td className="text-sm font-bold">{score.position}</td>
+      <td>{score.user.username}</td>
+      <td>{score.points}</td>
+      <td className="flex flex-col items-center justify-center">
+        <span className="inline text-sm">{performancePercentual}</span>
+        <meter
+          low={0.35}
+          high={0.7}
+          optimum={1}
+          min={0}
+          max={1}
+          value={score.performance}
+        >
+          {performancePercentual}
+        </meter>
+      </td>
+      <td>
+        <Link
+          to={`/u/${score.user.username}`}
+          className={linkClassNames({ type })}
+        >
+          <img src="/profile.svg" alt="profile" />
+        </Link>
+      </td>
+    </LeaderboardRowWrapper>
+  );
 }
--- src/screens/Leaderboard/index.jsx
+++ src/screens/Leaderboard/index.jsx
@@ -1,120 +1,119 @@
-import { useContext, useEffect } from 'react'
-import ReactLoading from 'react-loading'
+import { useContext, useEffect } from "react";
+import ReactLoading from "react-loading";
 
-import { ReactComponent as NoDataIllustration } from '~/assets/images/illustrations/no_data.svg'
-import { LeaderboardRow, LeaderboardRowDivider } from './components/LeaderboardRow'
-import { AppBar, ErrorAlert } from '~/components'
-import { useFetch } from '~/hooks'
-import { AuthContext } from '~/providers/AuthProvider'
+import { ReactComponent as NoDataIllustration } from "~/assets/images/illustrations/no_data.svg";
+import {
+  LeaderboardRow,
+  LeaderboardRowDivider,
+} from "./components/LeaderboardRow";
+import { AppBar, ErrorAlert } from "~/components";
+import { useFetch } from "~/hooks";
+import { AuthContext } from "~/providers/AuthProvider";
 
-import { leaderboardApi } from '~/domain/api'
+import { leaderboardApi } from "~/domain/api";
 
 function LeaderboardContent() {
-    const [state, isLoading, error, fetch] = useFetch({})
-    const [user] = useContext(AuthContext)
+  const [state, isLoading, error, fetch] = useFetch({});
+  const [user] = useContext(AuthContext);
 
-    async function fetchLeaderboard() {
-        const scoresLimit = 10
+  async function fetchLeaderboard() {
+    const scoresLimit = 20;
 
-        const scoresResult = await leaderboardApi.getLeaderboard()
-        const userScoreIndex = scoresResult
-            .findIndex(score => score.user.username === user.username)
+    const scoresResult = await leaderboardApi.getLeaderboard();
+    const userScoreIndex = scoresResult.findIndex(
+      (score) => score.user.username === user.username
+    );
 
-        const limitedScores = scoresResult.slice(0, scoresLimit)
+    const limitedScores = scoresResult.slice(0, scoresLimit);
 
-        let userScorePosition = -1
-        
-        // Se o usuário não estiver entre os 10 primeiros
-        // adiciona-o ao final da lista
-        if (userScoreIndex >= limitedScores.length) {
+    let userScorePosition = -1;
 
-            // Adiciona uma separação entre os demais itens e 
-            // o item do usuário, pois a sua posição não está 
-            // na sequência da lista.
-            // Exemplo:
-            // O último item da lista é o 10 e a posição 
-            // do usuário é o 15
-            if (userScoreIndex > limitedScores.length) {
-                limitedScores.push({type: 'divider'})
-            }
+    // Se o usuário não estiver entre os 10 primeiros
+    // adiciona-o ao final da lista
+    if (userScoreIndex >= limitedScores.length) {
+      // Adiciona uma separação entre os demais itens e
+      // o item do usuário, pois a sua posição não está
+      // na sequência da lista.
+      // Exemplo:
+      // O último item da lista é o 10 e a posição
+      // do usuário é o 15
+      if (userScoreIndex > limitedScores.length) {
+        limitedScores.push({ type: "divider" });
+      }
 
-            const userScore = scoresResult[userScoreIndex]
-            userScorePosition = userScore.position
-            limitedScores.push(userScore)
-        }
-        
-        return {
-            userScorePosition,
-            scores: limitedScores
-        }
+      const userScore = scoresResult[userScoreIndex];
+      userScorePosition = userScore.position;
+      limitedScores.push(userScore);
     }
 
-    useEffect(() => {
-        fetch(fetchLeaderboard)
-    }, [])
+    return {
+      userScorePosition,
+      scores: limitedScores,
+    };
+  }
 
-    if (error) {
-        return (
-            <ErrorAlert
-                error={error.message ?? 'Erro ao carregar a classificação'}
-            />
-        )
-    }
+  useEffect(() => {
+    fetch(fetchLeaderboard);
+  }, []);
 
-    if (isLoading) {
-        return <ReactLoading className="m-auto" type="bars" color="#AF053F" />
-    }
-
-    if (state.scores?.length === 0) {
-        return (
-            <div>
-                <p className="font-bold text-lg text-gray-400">
-                    Ainda não há classificação
-                </p>
-                <NoDataIllustration className="mt-4 h-56 m-auto w-full" />
-            </div>
-        )
-    }
-
+  if (error) {
     return (
-        <table cellPadding={8} className="w-full mb-8">
-            <thead>
-                <tr>
-                    <th title="Classificação">#</th>
-                    <th title="Usuário">Usuário</th>
-                    <th title="Pontuação">Pontuação</th>
-                    <th title="Aproveitamento">Aproveitamento</th>
-                    <th title="Opções"></th>
-                </tr>
-            </thead>
-            <tbody className="border border-slate-200 rounded-md">
-                {
-                    state.scores?.map(score => {
-                        if (score.type === 'divider') {
-                            return <LeaderboardRowDivider />
-                        }
+      <ErrorAlert error={error.message ?? "Erro ao carregar a classificação"} />
+    );
+  }
 
-                        return (
-                            <LeaderboardRow
-                                key={score.user.username ?? score.type}
-                                score={score}
-                                isCurrentUser={score.position === state?.userScorePosition}
-                            />
-                        )
-                    })
-                }
-            </tbody>
-        </table>
-    )
+  if (isLoading) {
+    return <ReactLoading className="m-auto" type="bars" color="#AF053F" />;
+  }
+
+  if (state.scores?.length === 0) {
+    return (
+      <div>
+        <p className="font-bold text-lg text-gray-400">
+          Ainda não há classificação
+        </p>
+        <NoDataIllustration className="mt-4 h-56 m-auto w-full" />
+      </div>
+    );
+  }
+
+  return (
+    <table cellPadding={2} className="w-full mb-8">
+      <thead>
+        <tr>
+          <th title="Classificação">#</th>
+          <th title="Usuário">Usuário</th>
+          <th title="Pontuação">Pontuação</th>
+          <th title="Aproveitamento">Aproveitamento</th>
+          <th title="Opções"></th>
+        </tr>
+      </thead>
+      <tbody className="border border-slate-200 rounded-md">
+        {state.scores?.map((score) => {
+          if (score.type === "divider") {
+            return <LeaderboardRowDivider />;
+          }
+
+          return (
+            <LeaderboardRow
+              key={score.user.username ?? score.type}
+              score={score}
+              isCurrentUser={score.position === state?.userScorePosition}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
 
 export function LeaderboardScreen() {
-    return (
-        <>
-            <AppBar title="Classificação" />
-            <main className="px-2 max-w-2xl container my-8 text-center overflow-x-auto">
-                <LeaderboardContent />
-            </main>
-        </>
-    )
+  return (
+    <>
+      <AppBar title="Classificação" />
+      <main className="px-2 max-w-2xl container my-8 text-center overflow-x-auto">
+        <LeaderboardContent />
+      </main>
+    </>
+  );
 }
